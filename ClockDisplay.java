3709cb81b7611f05562de2869451dc751e4e21d5
Manuel <manubp9@gmail.com>
2015-11-12 07:07:02 +0100

    Creados los atributos y el constructor que los inicializa a 0

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,33 +1,30 @@
 
-/**
- * Write a description of class ClockDisplay here.
+ /**Imaginemos que tenemos un amigo programador en Java como nosotros que quiere hacer una aplicación para teléfonos Android consistente en un reloj en formato 24 horas,
+ * que muestre las horas y minutos desde las 00:00 hasta las 23:59.
+ *Como anda muy apurado de tiempo, nos pide que le hagamos parte del trabajo. Concretamente necesita que le programemos una clase llamada ClockDisplay que:
+ *Permita fijar un momento determinado de tiempo expresado en horas y minutos. Por ejemplo, si yo le indico a un método 23 y 45 como parámetros,
+ *el tiempo del reloj debe quedar fijado a 23:45. El nombre del método debe ser setTime.
+ *permita hacer avanzar un minuto el tiempo fijado. Evidentemente, el usuario podrá invocar este método tantas veces como quiera. El nombre del método debe ser timeTick.
+ *Devolver una cadena de 5 caracteres consistente en la hora y los minutos separados por dos puntos. El nombre del método debe ser getTime. 
+ *Ojo: la cadena siempre debe tener 5 caracteres.
+ *Vete haciendo commits conforme le añades funcionalidad a tu clase y súbelo a GitHub.
+ *Es muy posible que no seas capaz de completar la actividad. No te preocupes, el objetivo es intentarlo.
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
-public class ClockDisplay
+    public class ClockDisplay
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    // 
+    private int horas;
+    // 
+    private int minutos;
+    
     /**
-     * Constructor for objects of class ClockDisplay
+     * iniciar valores a cero 
      */
     public ClockDisplay()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+        horas = 0;
+        minutos = 0;
+    } 
+}    
+   
